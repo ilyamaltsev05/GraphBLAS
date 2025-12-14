e4fd0286315975b3945f317c743323ed2c5c081f
Ilya Maltsev <malcevilya5@gmail.com>
2025-12-14 19:06:11 +0300

    feat: extended kronecker tests with mask computation

--- Test/test227.m
+++ Test/test227.m
@@ -16,6 +16,16 @@ dtn = struct ( 'inp0', 'tran' ) ;
 dnt = struct ( 'inp1', 'tran' ) ;
 dtt = struct ( 'inp0', 'tran', 'inp1', 'tran' ) ;
 
+dnn.mask = 'default' ;
+dtn.mask = 'default' ;
+dnt.mask = 'default' ;
+dtt.mask = 'default' ;
+
+dnn.outp = 'default' ;
+dtn.outp = 'default' ;
+dnt.outp = 'default' ;
+dtt.outp = 'default' ;
+
 types = { 'int32', 'int64', 'single', 'double' } ;
 
 am = 5 ;
@@ -28,8 +38,9 @@ Bx = sparse (100 * sprandn (bm,bn, 0.5)) ;
 cm = am * bm ;
 cn = an * bn ;
 Cx = sparse (cm,cn) ;
-AT = Ax' ;
-BT = Bx' ;
+Maskmat = sprandn (cm,cn,0.2) ~= 0 ;
+ATmat = Ax' ;
+BTmat = Bx' ;
 
 for k2 = [4 7 45:52 ]
     for k1 = 1:4
@@ -64,8 +75,18 @@ for k2 = [4 7 45:52 ]
                             B.is_csc   = B_is_csc   ;
 
                             clear C
-                            C.matrix = Cx ;
+                            C.matrix = sparse (cm,cn) ;
                             C.is_csc   = C_is_csc   ;
+
+                            clear AT
+                            AT.matrix = ATmat ;
+                            AT.is_hyper = A_is_hyper ;
+                            AT.is_csc = A.is_csc ;
+
+                            clear BT
+                            BT.matrix = BTmat ;
+                            BT.is_hyper = B_is_hyper ;
+                            BT.is_csc = B.is_csc ;
 
                             %---------------------------------------
                             % kron(A,B)
@@ -103,6 +124,47 @@ for k2 = [4 7 45:52 ]
                             C1 = GB_mex_kron  (C, [ ], [ ], op, AT, BT, dtt) ;
                             GB_spec_compare (C0, C1) ;
 
+                            % tests with Mask
+                            for Mask_is_hyper = 0:1
+                            for Mask_is_csc = 0:1
+                            fprintf('*')
+
+                            A.is_csc = A_is_csc ;
+                            B.is_csc = B_is_csc ;
+                            A.is_hyper = A_is_hyper ;
+                            B.is_hyper = B_is_hyper ;
+
+                            clear M
+                            M.matrix = Maskmat ;
+                            M.is_hyper = Mask_is_hyper ;
+                            M.is_csc = Mask_is_csc;
+                            C.is_csc = C_is_csc ;
+
+                            % kron(A, B) with Mask
+                            C0 = GB_spec_kron (C, M, [ ], op, A, B, dnn) ;
+                            fprintf('#') ;
+                            C1 = GB_mex_kron (C, M, [ ], op, A, B, dnn) ;
+                            %full(C0.matrix)
+                            %full(C1.matrix)
+                            GB_spec_compare(C0, C1) ;
+
+                            % kron(A', B) with Mask
+                            C0 = GB_spec_kron (C, M, [ ], op, AT, B, dtn) ;
+                            C1 = GB_mex_kron  (C, M, [ ], op, AT, B, dtn) ;
+                            GB_spec_compare (C0, C1) ;
+
+                            % kron(A, B') with Mask
+                            C0 = GB_spec_kron (C, M, [ ], op, A, BT, dnt) ;
+                            C1 = GB_mex_kron  (C, M, [ ], op, A, BT, dnt) ;
+                            GB_spec_compare (C0, C1) ;
+
+                            % kron(A', B') with Mask
+                            C0 = GB_spec_kron (C, M, [ ], op, AT, BT, dtt) ;
+                            C1 = GB_mex_kron  (C, M, [ ], op, AT, BT, dtt) ;
+                            GB_spec_compare (C0, C1) ;
+
+                            end
+                            end
                         end
                     end
                 end
